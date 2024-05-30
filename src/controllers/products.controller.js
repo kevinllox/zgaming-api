fd530c2e031b35cb491d1ab90f852af2f4dc6a0b
Kevin Grande <kevinbussines2020@gmail.com>
2024-05-30 16:38:00 -0600

    REFACTOR: products controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,13 +1,163 @@
-const getProducts = (req, res) => {
-  return res.status(200).json([
-    { id: 1, name: "Keyboard Logitech" },
-    { id: 2, name: "GTX 1660 SUPER" },
-  ]);
-};
+import { PrismaClient } from "@prisma/client";
 
-const getProductById = (req, res) => {
+const prisma = new PrismaClient();
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await prisma.producto.findMany();
+    res.status(200).json(products);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error when searching for product", error });
+  }
+};
+const getProductById = async (req, res) => {
   const { id } = req.params;
-  return res.status(200).json([{ id: id, name: "Keyboard Logitech" }]);
+  try {
+    const productById = await prisma.producto.findUnique({
+      where: {
+        idProducto: parseInt(id),
+      },
+    });
+    res.status(200).json(productById);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `We couldn't find this id: ${id} in products`, error });
+  }
+};
+const getProductByCategory = async (req, res) => {
+  const { idCategoria } = req.params;
+
+  try {
+    const products = await prisma.producto.findMany({
+      where: {
+        idCategoria: parseInt(idCategoria),
+      },
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Error when searching for product" });
+  }
 };
 
-export { getProducts, getProductById };
+const addProduct = async (req, res) => {
+  const {
+    nombreProducto,
+    descripcion,
+    precio,
+    stock,
+    imagenProducto,
+    isFavorito,
+    idCategoria,
+  } = req.body;
+  try {
+    const category = await prisma.categoria.findUnique({
+      where: {
+        idCategoria: idCategoria,
+      },
+    });
+    if (!category) {
+      return res.status(400).json({ error: "Category not found" });
+    }
+    const product = await prisma.producto.create({
+      data: {
+        nombreProducto,
+        descripcion,
+        precio,
+        stock,
+        imagenProducto,
+        isFavorito,
+        idCategoria,
+      },
+    });
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "The product cannot be created", error });
+  }
+};
+
+const updateProductById = async (req, res) => {
+  const { idProducto } = req.params;
+  const {
+    nombreProducto,
+    descripcion,
+    precio,
+    stock,
+    imagenProducto,
+    isFavorito,
+    idCategoria,
+  } = req.body;
+  try {
+    const findProduct = await prisma.producto.findUnique({
+      where: {
+        idProducto: parseInt(idProducto),
+      },
+    });
+    if (!findProduct) {
+      return res.status(404).json({ error: "product not found" });
+    }
+    const updateProduct = await prisma.producto.update({
+      where: { idProducto: parseInt(idProducto) },
+      data: {
+        nombreProducto,
+        descripcion,
+        precio,
+        stock,
+        imagenProducto,
+        isFavorito,
+        idCategoria,
+      },
+    });
+    res.status(201).json(updateProduct);
+  } catch (error) {
+    res.status(500).json({ error: "Error while updating the product" });
+  }
+};
+
+const getFavorites = async (req, res) => {
+  try {
+    const favorites = await prisma.producto.findMany({
+      where: {
+        isFavorito: true,
+      },
+    });
+    res.status(200).json(favorites);
+  } catch (error) {
+    res.status(500).json({ error: "Error while getting favorites products" });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const { idProducto } = req.params;
+
+  try {
+    const findProduct = await prisma.producto.findUnique({
+      where: {
+        idProducto: parseInt(idProducto),
+      },
+    });
+    if (!findProduct) {
+      return res.status(404).json({ error: "product not found" });
+    }
+    const product = await prisma.producto.delete({
+      where: {
+        idProducto: parseInt(idProducto),
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Error when deleting the product" });
+  }
+};
+
+export {
+  getProducts,
+  getProductById,
+  getProductByCategory,
+  addProduct,
+  updateProductById,
+  getFavorites,
+  deleteProduct,
+};
--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import authRoutes from "./router/auth.router.js";
+import categoriesRoutes from "./router/categories.router.js";
+import productsRoutes from "./router/products.router.js";
+import purchaseDetailsRoutes from "./router/purchaseDetails.router.js";
 
-import express from 'express';
-import cors from 'cors'
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import authRoutes from './router/auth.router.js'
-import categoriesRoutes from './router/categories.router.js'
-import productsRoutes from './router/products.router.js'
-import purchaseDetailsRoutes from './router/purchaseDetails.router.js'
-
-
-const app = express()
-app.use(cors({origin: "http://localhost:3000", credentials: true}));
+const app = express();
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use("/api", authRoutes);
 app.use("/api", productsRoutes);
 app.use("/api", categoriesRoutes);
 app.use("/api", purchaseDetailsRoutes);
 
-
 const server = app.listen(3000, () =>
   console.log(`
-🚀 Server ready at: http://localhost:3000
-⭐️ See sample requests: http://pris.ly/e/js/rest-express#3-using-the-rest-api`),
-)
+🚀 Server ready at: http://localhost:3000`)
+);
