c1230b4c9eb85b7813b9477351ed2b8e3051b1ad
Kevin Grande <kevinbussines2020@gmail.com>
2024-05-31 08:54:48 -0600

    add jsonwebtoken

---
+++ libs/jsonwebtoken.js
@@ -0,0 +1,21 @@
+const jsonwebtoken = require("jsonwebtoken");
+const privateKey = process.env.TOKEN_SECRET;
+
+const createAccessToken = (payload) => {
+  return new Promise((resolve, reject) => {
+    jsonwebtoken.sign(
+      payload,
+      privateKey,
+      {
+        expiresIn: "1d",
+      },
+      (error, token) => {
+        if (error) reject(error);
+        resolve(token);
+      }
+    );
+  });
+};
+
+
+export default createAccessToken;
