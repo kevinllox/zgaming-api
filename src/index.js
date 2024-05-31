258d3202e0f8657d5dc7ec085f8b8e984f3c1a49
Kevin Grande <kevinbussines2020@gmail.com>
2024-05-30 19:04:58 -0600

    ADD vercel file

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import productsRoutes from "./router/products.router.js";
 import purchaseDetailsRoutes from "./router/purchaseDetails.router.js";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin:process.env.BASE_URL, credentials: true }));
 
 
 const env = process.env.NODE_ENV || 'development';
